7a10187403d404332a42f1ce333ffbecbc741e37
Giacca90 <giacca90mobile@gmail.com>
2024-06-19 22:28:24 +0200

    Creada media home

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      keyframes: {
-        slideIn: {
-          "0%": { transform: "translateX(1300%)" },
-          "100%": { transform: "translateX(-200%)" },
-        },
-      },
-      animation: {
-        slideIn: "slideIn 10s ease-out infinite",
-      },
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			keyframes: {
+				slideIn: {
+					'0%': { transform: 'translateX(1300%)' },
+					'100%': { transform: 'translateX(-200%)' },
+				},
+			},
+			animation: {
+				slideIn: 'slideIn 40s ease-out infinite',
+			},
+		},
+	},
+	plugins: [],
 };
