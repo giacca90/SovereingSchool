b385522b0768d41e66a66ef599358e3e2d3bd761
Giacca90 <giacca90mobile@gmail.com>
2024-06-23 01:14:34 +0200

    Intento de arreglar la vista

--- front/src/app/components/cursos/cursos.component.ts
+++ front/src/app/components/cursos/cursos.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
-  selector: 'app-cursos',
-  standalone: true,
-  imports: [],
-  templateUrl: './cursos.component.html',
-  styleUrl: './cursos.component.css'
+	selector: 'app-cursos',
+	standalone: true,
+	imports: [],
+	templateUrl: './cursos.component.html',
+	styleUrl: './cursos.component.css',
 })
 export class CursosComponent {
-
+	constructor(public loginService: LoginService) {}
 }
