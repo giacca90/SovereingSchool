8c9d8284ef08c3bec3efecc6baf9e7d560d23152
Giacca90 <giacca90mobile@gmail.com>
2024-07-10 13:04:46 +0200

    Resuelto error de carga

--- front/src/app/components/curso/curso.component.ts
+++ front/src/app/components/curso/curso.component.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-unsafe-optional-chaining */
-import { afterNextRender, Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Curso } from '../../models/Curso';
 import { CursosService } from '../../services/cursos.service';
@@ -15,79 +15,33 @@ import { UsuariosService } from '../../services/usuarios.service';
 })
 export class CursoComponent {
 	private id_curso: number = 0;
-	public curso: Curso | null;
+	public curso: Curso | null = null;
 	public nombresProfesores: string | undefined = '';
 
 	constructor(
 		private route: ActivatedRoute,
 		private cursoService: CursosService,
 		private usuarioService: UsuariosService,
+		private cdr: ChangeDetectorRef,
 		public loginService: LoginService,
 	) {
 		this.route.params.subscribe((params) => {
 			this.id_curso = params['id_curso'];
 		});
 
-		this.curso = this.cursoService.getCurso(this.id_curso);
-		if (this.curso) {
-			if (this.curso.profesores_curso.length == 1) this.nombresProfesores = this.usuarioService.getNombreProfe(this.curso.profesores_curso[0]);
-			else {
-				let nombres: string | undefined = this.usuarioService.getNombreProfe(this.curso.profesores_curso[0])?.toString();
-				for (let i = 1; i < this.curso.profesores_curso.length; i++) {
-					nombres = nombres + ' y ' + this.usuarioService.getNombreProfe(this.curso.profesores_curso[i]);
+		this.cursoService.getCurso(this.id_curso).then((curso) => {
+			this.curso = curso;
+			if (this.curso) {
+				console.log('Recibido el curso: ' + this.curso.descriccion_larga);
+				if (this.curso.profesores_curso.length == 1) this.nombresProfesores = this.usuarioService.getNombreProfe(this.curso.profesores_curso[0]);
+				else {
+					let nombres: string | undefined = this.usuarioService.getNombreProfe(this.curso.profesores_curso[0])?.toString();
+					for (let i = 1; i < this.curso.profesores_curso.length; i++) {
+						nombres = nombres + ' y ' + this.usuarioService.getNombreProfe(this.curso.profesores_curso[i]);
+					}
+					this.nombresProfesores = nombres;
 				}
-				this.nombresProfesores = nombres;
 			}
-
-			afterNextRender(() => {
-				const divClases: HTMLDivElement = document.getElementById('clases') as HTMLDivElement;
-				if (this.curso) {
-					if (this.curso.clases_curso) {
-						for (const clase of this.curso.clases_curso) {
-							const divInt: HTMLDivElement = document.createElement('div') as HTMLDivElement;
-							divInt.classList.add('flex-grow', 'p-2', 'items-start', 'border', 'border-black', 'rounded-lg');
-							const p: HTMLParagraphElement = document.createElement('p');
-							p.innerHTML = `${clase.posicion_clase}: ${clase.nombre_clase}`;
-							divInt.appendChild(p);
-							const desc: HTMLParagraphElement = document.createElement('p');
-							desc.textContent = clase.descriccion_clase.toString();
-							divInt.appendChild(desc);
-							divClases.appendChild(divInt);
-							divClases.appendChild(document.createElement('br'));
-						}
-					}
-
-					if (this.curso.planes_curso && this.curso.planes_curso.length > 0) {
-						const p: HTMLParagraphElement = document.createElement('p');
-						p.innerHTML = 'Este curso es parte de los siguiente planes:';
-						divClases.appendChild(p);
-						for (const plan of this.curso.planes_curso) {
-							const p: HTMLParagraphElement = document.createElement('p');
-							p.innerHTML = plan.nombre_plan.toString();
-							divClases.appendChild(p);
-						}
-					}
-					if (this.loginService.usuario) {
-						const boton: HTMLButtonElement = document.getElementById('boton') as HTMLButtonElement;
-						if (this.loginService.usuario?.cursos_usuario)
-							for (const cursoUsuario of this.loginService.usuario?.cursos_usuario) {
-								if (cursoUsuario.id_curso == this.curso.id_curso) {
-									boton.textContent = 'Ir al curso';
-									break;
-								}
-							}
-						if (this.curso.planes_curso) {
-							for (const planes of this.curso.planes_curso) {
-								if (planes.id_plan == this.loginService.usuario?.plan_usuario) {
-									boton.textContent = 'Ir al curso';
-									const nota: HTMLParagraphElement = document.getElementById('nota') as HTMLParagraphElement;
-									nota.innerHTML = 'Este curso es parte de tu Plan ' + planes.nombre_plan;
-								}
-							}
-						}
-					}
-				}
-			});
-		}
+		});
 	}
 }
--- front/src/app/services/cursos.service.ts
+++ front/src/app/services/cursos.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 import { Curso } from '../models/Curso';
 
 @Injectable({
@@ -11,35 +12,25 @@ export class CursosService {
 
 	constructor(private http: HttpClient) {}
 
-	getCurso(id_curso: number) {
-		for (const curso of this.cursos) {
-			if (curso.id_curso == id_curso) {
-				if (!curso.clases_curso) {
-					this.http.get<Curso>(this.backURL + '/cursos/getCurso/' + id_curso).subscribe({
-						next: (response) => {
-							this.cursos[this.cursos.findIndex((curso) => curso.id_curso === id_curso)] = response;
-						},
-						error(e: Error) {
-							console.error('Error en cargar curso: ' + e.message);
-						},
-					});
-				}
-				return curso;
+	async getCurso(id_curso: number) {
+		for (let i = 0; i < this.cursos.length; i++) {
+			if (this.cursos[i].id_curso == id_curso) {
+				if (this.cursos[i].clases_curso === undefined) {
+					try {
+						const response = await firstValueFrom(this.http.get<Curso>(`${this.backURL}/cursos/getCurso/${id_curso}`));
+						this.cursos[i].clases_curso = response.clases_curso;
+						this.cursos[i].descriccion_larga = response.descriccion_larga;
+						this.cursos[i].fecha_publicacion_curso = response.fecha_publicacion_curso;
+						this.cursos[i].planes_curso = response.planes_curso;
+						this.cursos[i].precio_curso = response.precio_curso;
+						return this.cursos[i];
+					} catch (error) {
+						console.error('Error en cargar curso:', error);
+						return null;
+					}
+				} else return this.cursos[i];
 			}
 		}
 		return null;
 	}
-
-	/* private async cargaCursos() {
-		this.http.get<Curso[]>(this.backURL + '/cursos/getAll').subscribe({
-			next: (response) => {
-				response.map((curso: Curso) => {
-					this.cursos.push(curso);
-				});
-			},
-			error: (error: HttpErrorResponse) => {
-				console.error('HTTP request failed:', error);
-			},
-		});
-	} */
 }
