5ca326716612a7c301fc6fe7f10362d27cdb558e
Giacca90 <giacca90mobile@gmail.com>
2024-07-12 17:05:51 +0200

    Segunda parte del componente editor con drag and drop

--- front/src/app/components/editor-curso/editor-curso.component.ts
+++ front/src/app/components/editor-curso/editor-curso.component.ts
@@ -27,36 +27,51 @@ export class EditorCursoComponent {
 	onDragStart(event: Event, id: number) {
 		console.log('Start');
 		const event2: DragEvent = event as DragEvent;
+		const div = event2.target as HTMLDivElement;
+		// Crear una copia del div original
+		const img = div.cloneNode(true) as HTMLDivElement;
+
+		// Copiar las clases del div original a la copia
+		img.className = div.className;
+
+		// Añadir la copia al documento de forma temporal para que setDragImage funcione
+		img.style.position = 'absolute';
+		img.style.top = '-9999px';
+		document.body.appendChild(img);
 		this.draggedElementId = id;
 		event2.dataTransfer?.setData('text/plain', id.toString());
-		event2.dataTransfer?.setDragImage(event.target as HTMLElement, 0, 0);
+		event2.dataTransfer?.setDragImage(img, 0, 0);
+		setTimeout(() => {
+			document.body.removeChild(img);
+		}, 0);
+		div.classList.add('opacity-0');
 	}
 
-	onDragOver(event: Event) {
+	onDragOver(event: Event, id: number) {
 		event.preventDefault();
 		const event2: DragEvent = event as DragEvent;
 		console.log('Over ' + this.getClosestElementId(event2));
+		if (this.draggedElementId === null || this.draggedElementId === id) {
+			return;
+		}
+
+		if (this.curso?.clases_curso) {
+			const draggedIndex = this.curso.clases_curso.findIndex((clase) => clase.id_clase === this.draggedElementId);
+			const targetIndex = this.curso.clases_curso.findIndex((clase) => clase.id_clase === id);
+
+			if (draggedIndex > -1 && targetIndex > -1 && draggedIndex !== targetIndex) {
+				const draggedClase = this.curso.clases_curso[draggedIndex];
+				const temp: number = draggedClase.posicion_clase;
+				draggedClase.posicion_clase = this.curso.clases_curso[targetIndex].posicion_clase;
+				this.curso.clases_curso[targetIndex].posicion_clase = temp;
+				this.curso.clases_curso.splice(draggedIndex, 1);
+				this.curso.clases_curso.splice(targetIndex, 0, draggedClase);
+			}
+		}
 	}
 
 	onDrop(event: Event) {
-		const event2: DragEvent = event as DragEvent;
 		event.preventDefault();
-		const targetId = this.getClosestElementId(event2);
-		if (this.draggedElementId !== null && targetId !== null && this.draggedElementId !== targetId && this.curso && this.curso.clases_curso) {
-			const draggedIndex = this.curso.clases_curso.findIndex((clase) => clase.id_clase === this.draggedElementId);
-			const targetIndex = this.curso.clases_curso.findIndex((clase) => clase.id_clase === targetId);
-
-			if (draggedIndex > -1 && targetIndex > -1) {
-				const draggedClase = this.curso.clases_curso[draggedIndex];
-				this.curso.clases_curso.splice(draggedIndex, 1);
-				this.curso.clases_curso.splice(targetIndex, 0, draggedClase);
-				//Cambia la posición del elemento
-				const temp: number = this.curso.clases_curso[draggedIndex].posicion_clase;
-				this.curso.clases_curso[draggedIndex].posicion_clase = this.curso.clases_curso[targetId].posicion_clase;
-				this.curso.clases_curso[targetId].posicion_clase = temp;
-			}
-		}
-
 		this.draggedElementId = null;
 	}
 
@@ -77,7 +92,10 @@ export class EditorCursoComponent {
 		).element;
 		return closestElement ? parseInt(closestElement.id.split('-')[1], 10) : null;
 	}
-	onDragEnd(): void {
+
+	onDragEnd(event: Event): void {
+		const event2 = event as DragEvent;
+		(event2.target as HTMLDivElement).classList.remove('opacity-0');
 		this.draggedElementId = null;
 	}
 }
