f7b3e9fe92717d9b0fec49568b836606f244f55b
Giacca90 <giacca90mobile@gmail.com>
2024-07-12 17:58:17 +0200

    Completado componente editor con drag and drop (falta hacer los cambios en el back

--- front/src/app/components/editor-curso/editor-curso.component.ts
+++ front/src/app/components/editor-curso/editor-curso.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Curso } from '../../models/Curso';
 import { CursosService } from '../../services/cursos.service';
 
@@ -10,31 +11,59 @@ import { CursosService } from '../../services/cursos.service';
 	templateUrl: './editor-curso.component.html',
 	styleUrl: './editor-curso.component.css',
 })
-export class EditorCursoComponent {
+export class EditorCursoComponent implements OnInit, OnDestroy {
+	private subscription: Subscription = new Subscription();
 	id_curso: number = 0;
 	curso: Curso | null = null;
+	cursoOriginal: Curso | null = null;
 	draggedElementId: number | null = null;
+	editado: boolean = false;
+
 	constructor(
-		public cursoService: CursosService,
 		private route: ActivatedRoute,
+		private router: Router,
+		public cursoService: CursosService,
 	) {
 		this.route.params.subscribe((params) => {
 			this.id_curso = params['id_curso'];
-			this.cursoService.getCurso(this.id_curso).then((curso) => (this.curso = curso));
+			this.cursoService.getCurso(this.id_curso).then((curso) => {
+				this.curso = curso;
+				this.cursoOriginal = JSON.parse(JSON.stringify(curso));
+			});
 		});
+	}
+
+	ngOnInit(): void {
+		this.subscription.add(
+			this.router.events.subscribe((event) => {
+				if (event instanceof NavigationStart && this.editado) {
+					this.cursoService.getCurso(this.id_curso).then((curso) => (this.curso = curso));
+
+					const userConfirmed = window.confirm('Tienes cambios sin guardar. ¿Estás seguro de que quieres salir?');
+					if (!userConfirmed) {
+						this.router.navigateByUrl(this.router.url); // Mantén al usuario en la misma página
+					}
+				}
+			}),
+		);
+	}
+	ngOnDestroy(): void {
+		this.subscription.unsubscribe();
+	}
+
+	@HostListener('window:beforeunload', ['$event'])
+	unloadNotification($event: { returnValue: string }): void {
+		if (this.editado) {
+			$event.returnValue = 'Tienes cambios sin guardar. ¿Estás seguro de que quieres salir?';
+		}
 	}
 
 	onDragStart(event: Event, id: number) {
 		console.log('Start');
 		const event2: DragEvent = event as DragEvent;
 		const div = event2.target as HTMLDivElement;
-		// Crear una copia del div original
 		const img = div.cloneNode(true) as HTMLDivElement;
-
-		// Copiar las clases del div original a la copia
 		img.className = div.className;
-
-		// Añadir la copia al documento de forma temporal para que setDragImage funcione
 		img.style.position = 'absolute';
 		img.style.top = '-9999px';
 		document.body.appendChild(img);
@@ -61,6 +90,7 @@ export class EditorCursoComponent {
 
 			if (draggedIndex > -1 && targetIndex > -1 && draggedIndex !== targetIndex) {
 				const draggedClase = this.curso.clases_curso[draggedIndex];
+				this.compruebaCambios();
 				const temp: number = draggedClase.posicion_clase;
 				draggedClase.posicion_clase = this.curso.clases_curso[targetIndex].posicion_clase;
 				this.curso.clases_curso[targetIndex].posicion_clase = temp;
@@ -97,5 +127,9 @@ export class EditorCursoComponent {
 		const event2 = event as DragEvent;
 		(event2.target as HTMLDivElement).classList.remove('opacity-0');
 		this.draggedElementId = null;
+	}
+
+	compruebaCambios() {
+		this.editado = JSON.stringify(this.curso) === JSON.stringify(this.cursoOriginal);
 	}
 }
