7ca1c217e6ff964427202cbc924d9763b871520c
Giacca90 <giacca90mobile@gmail.com>
2024-08-13 17:34:25 +0200

    Algunas mejoras de rendimiento

--- front/src/app/components/home/home.component.ts
+++ front/src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
-import { afterNextRender, ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Curso } from '../../models/Curso';
 import { Usuario } from '../../models/Usuario';
 import { CursosService } from '../../services/cursos.service';
 import { InitService } from '../../services/init.service';
@@ -18,90 +17,12 @@ export class HomeComponent {
 	anchoVista: number = 0;
 	vista: HTMLDivElement | null = null;
 	constructor(
-		private cursoService: CursosService,
+		public cursoService: CursosService,
 		private usuarioService: UsuariosService,
 		public initService: InitService,
 		private cdr: ChangeDetectorRef,
 		public router: Router,
-	) {
-		this.cursoService.cursos.forEach((curso: Curso) => {
-			const div: HTMLDivElement = document.createElement('div');
-			div.classList.add('relative', 'm-2', 'border', 'border-black', 'cursor-pointer');
-			div.tabIndex = 0;
-			const fondo: HTMLDivElement = document.createElement('div');
-			fondo.classList.add('absolute', 'inset-0', 'bg-cover', 'bg-center', 'opacity-20', 'transition-opacity', 'duration-300', 'hover:opacity-30');
-			fondo.style.backgroundImage = `url('${curso.imagen_curso}')`;
-			div.appendChild(fondo);
-
-			const nombre: HTMLHeadingElement = document.createElement('h4');
-			nombre.classList.add('text-center', 'font-bold', 'text-blue-700');
-			nombre.textContent = curso.nombre_curso;
-			div.appendChild(nombre);
-
-			curso.profesores_curso.forEach((prof: Usuario) => {
-				const profeId = prof.id_usuario;
-				const divProfe: HTMLDivElement = document.createElement('div');
-				divProfe.classList.add('m-3', 'flex', 'items-center', 'justify-between');
-				const span: HTMLSpanElement = document.createElement('span');
-				const profe: Usuario | undefined = this.usuarioService.profes.find((usuario) => usuario.id_usuario === profeId);
-				if (profe) {
-					span.textContent = 'Por: ' + profe?.nombre_usuario;
-					divProfe.appendChild(span);
-					const logo: HTMLImageElement = document.createElement('img') as HTMLImageElement;
-					logo.classList.add('h-12', 'w-12', 'object-contain');
-					logo.src = profe.foto_usuario[0];
-					logo.alt = 'Logo';
-					divProfe.appendChild(logo);
-					div.appendChild(divProfe);
-				}
-			});
-
-			const desc: HTMLParagraphElement = document.createElement('p');
-			desc.classList.add('ml-3', 'mr-3');
-			desc.textContent = curso.descriccion_corta as string;
-			div.appendChild(desc);
-
-			div.addEventListener('click', () => {
-				this.router.navigate(['/curso/' + curso.id_curso]);
-			});
-			div.addEventListener('keydown', (event) => {
-				if (event.key === 'Enter') {
-					this.router.navigate(['/curso/' + curso.id_curso]);
-				}
-			});
-
-			this.vistaCursos.push(div);
-		});
-
-		afterNextRender(() => {
-			this.anchoVista = window.innerWidth;
-			this.vista = document.getElementById('carousel') as HTMLDivElement;
-			let numeroVistas: number;
-			if (this.anchoVista < 640) {
-				numeroVistas = 2;
-			} else if (this.anchoVista >= 640 && this.anchoVista < 768) {
-				numeroVistas = 3;
-			} else if (this.anchoVista >= 768 && this.anchoVista < 1024) {
-				numeroVistas = 4;
-			} else if (this.anchoVista >= 1024 && this.anchoVista < 1280) {
-				numeroVistas = 4;
-			} else if (this.anchoVista >= 1280 && this.anchoVista < 1536) {
-				numeroVistas = 4;
-			} else {
-				numeroVistas = 5;
-			}
-
-			let numeroRealVistas = numeroVistas;
-			if (this.cursoService.cursos.length < numeroVistas) numeroRealVistas = this.cursoService.cursos.length;
-
-			for (let i = 0; i < numeroRealVistas; i++) {
-				this.vista?.appendChild(this.vistaCursos[i]);
-			}
-
-			this.cdr.detectChanges();
-			this.carouselProfes();
-		});
-	}
+	) {}
 
 	async carouselProfes() {
 		const carouselProfes: HTMLDivElement[] = [];
