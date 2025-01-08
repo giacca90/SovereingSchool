259a04e6c77aa9a1b6ee645a5c2d397a3d5987a5
giacca90 <giacca90mobile@gmail.com>
2025-01-08 17:51:37 +0100

    Implementado el nuevo comando ffmpeg para la conversión de videos estáticos (falta probar con los streaming y mejorarlo utilizando executors)

--- back_streaming/src/main/java/com/sovereingschool/back_streaming/Services/StreamingService.java
+++ back_streaming/src/main/java/com/sovereingschool/back_streaming/Services/StreamingService.java
@@ -32,38 +32,6 @@ import com.sovereingschool.back_streaming.Repositories.ClaseRepository;
 @Service
 @Transactional
 public class StreamingService {
-    private static int[] getVideoResolutionAndFps(String inputFileName) throws Exception {
-        ProcessBuilder processBuilder = new ProcessBuilder("ffprobe",
-                "-v", "error",
-                "-select_streams", "v:0",
-                "-show_entries", "stream=width,height,r_frame_rate",
-                "-of", "csv=p=0", inputFileName);
-        Process process = processBuilder.start();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line;
-        int width = 0;
-        int height = 0;
-        int fps = 0;
-
-        while ((line = reader.readLine()) != null) {
-            System.out.println("line: " + line);
-
-            String[] parts = line.split(",");
-            width = Integer.parseInt(parts[0]);
-            height = Integer.parseInt(parts[1]);
-
-            // Para calcular los fps, "r_frame_rate" devuelve un formato como "30000/1001"
-            String[] frameRateParts = parts[2].split("/");
-            if (frameRateParts.length == 2) {
-                // Redondear el fps a entero
-                fps = (int) Math.round(Double.parseDouble(frameRateParts[0]) / Double.parseDouble(frameRateParts[1]));
-            }
-            break;
-        }
-        process.waitFor();
-        return new int[] { width, height, fps };
-    }
-
     private final Map<String, Process> ffmpegProcesses = new ConcurrentHashMap<>();
     private final Map<String, Process> previewProcesses = new ConcurrentHashMap<>();
 
@@ -78,9 +46,11 @@ public class StreamingService {
      * Función para convertir los videos de un curso
      * 
      * @param curso
+     * @throws IOException
+     * @throws InterruptedException
      */
     @Async
-    public void convertVideos(Curso curso) {
+    public void convertVideos(Curso curso) throws IOException, InterruptedException {
         if (curso.getClases_curso() != null && !curso.getClases_curso().isEmpty()) {
             for (Clase clase : curso.getClases_curso()) {
                 Path base = Paths.get(baseUploadDir.toString(), curso.getId_curso().toString(),
@@ -104,152 +74,15 @@ public class StreamingService {
                         System.err.println("Error al mover el video a la carpeta de destino");
                         continue;
                     }
-
-                    String inputFileName = destino.getName();
-
                     // TODO: prueba la generación del comando FFMPEG
+                    List<String> ffmpegCommand = null;
                     try {
-                        this.creaComandoFFmpeg(destino.getAbsolutePath(), false);
+                        ffmpegCommand = this.creaComandoFFmpeg(destino.getAbsolutePath(), false);
                     } catch (IOException | InterruptedException e) {
                         // TODO Auto-generated catch block
                         System.err.println("Error al generar el comando FFmpeg: " + e.getMessage());
                     }
-
-                    try {
-                        // Obtener la resolución del video
-                        int[] resolution = getVideoResolutionAndFps(destino.toString());
-                        int width = resolution[0];
-                        int height = resolution[1];
-                        int fps = resolution[2];
-                        System.out.println("Resolución: " + width + "x" + height);
-
-                        // Construcción del comando dependiendo de la resolución
-                        List<String> comando = new ArrayList<>();
-                        List<String> maps = new ArrayList<>();
-                        List<String> audio = new ArrayList<>();
-                        List<String> lista = new ArrayList<>();
-
-                        // Construcción de comandos según la resolución
-                        if (width >= 1920 && height >= 1080) {
-                            comando.add(
-                                    "[0:v]split=4[v1][v2][v3][v4]; [v1]copy[v1out]; [v2]scale=w=1280:h=720[v2out]; [v3]scale=w=854:h=480[v3out]; [v4]scale=w=640:h=360[v4out]");
-                            maps.addAll(Arrays.asList(
-                                    // Resolución 1080p
-                                    "-map", "[v1out]", "-c:v:0", "libx264", "-b:v:0", "5M", "-maxrate:v:0", "5M",
-                                    "-minrate:v:0", "5M", "-bufsize:v:0", "10M", "-preset", "fast", "-g", "48",
-                                    "-sc_threshold", "0", "-keyint_min", "48", "-hls_segment_filename",
-                                    "stream_%v/data%02d.ts",
-                                    "-hls_base_url", "stream_0/",
-                                    // Resolución 720p
-                                    "-map", "[v2out]", "-c:v:1", "libx264", "-b:v:1", "3M", "-maxrate:v:1", "3M",
-                                    "-minrate:v:1", "3M", "-bufsize:v:1", "3M", "-preset", "fast", "-g", "48",
-                                    "-sc_threshold", "0", "-keyint_min", "48", "-hls_segment_filename",
-                                    "stream_%v/data%02d.ts",
-                                    "-hls_base_url", "stream_1/",
-                                    // Resolución 480p
-                                    "-map", "[v3out]", "-c:v:2", "libx264", "-b:v:2", "1M", "-maxrate:v:2", "1M",
-                                    "-minrate:v:2", "1M", "-bufsize:v:2", "1M", "-preset", "fast", "-g", "48",
-                                    "-sc_threshold", "0", "-keyint_min", "48", "-hls_segment_filename",
-                                    "stream_%v/data%02d.ts",
-                                    "-hls_base_url", "stream_2/",
-                                    // Resolución 360p
-                                    "-map", "[v4out]", "-c:v:3", "libx264", "-b:v:3", "512k", "-maxrate:v:3", "512k",
-                                    "-minrate:v:3", "512k", "-bufsize:v:3", "1M", "-preset", "fast", "-g", "48",
-                                    "-sc_threshold", "0", "-keyint_min", "48", "-hls_segment_filename",
-                                    "stream_%v/data%02d.ts",
-                                    "-hls_base_url", "stream_3/"));
-                            lista.add("v:0,a:0");
-                            lista.add("v:1,a:1");
-                            lista.add("v:2,a:2");
-                            lista.add("v:3,a:3");
-                            audio.addAll(Arrays.asList("-map", "a:0", "-c:a:0", "aac", "-b:a:0", "96k", "-ac", "2"));
-                            audio.addAll(Arrays.asList("-map", "a:0", "-c:a:1", "aac", "-b:a:1", "96k", "-ac", "2"));
-                            audio.addAll(Arrays.asList("-map", "a:0", "-c:a:2", "aac", "-b:a:2", "96k", "-ac", "2"));
-                            audio.addAll(Arrays.asList("-map", "a:0", "-c:a:3", "aac", "-b:a:3", "48k", "-ac", "2"));
-                        } else if (width >= 1280 && height >= 720) {
-                            comando.add(
-                                    "[0:v]split=3[v1][v2][v3]; [v1]copy[v1out]; [v2]scale=w=854:h=480[v2out]; [v3]scale=w=640:h=360[v3out]");
-                            maps.addAll(Arrays.asList(
-                                    // Resolución 720p
-                                    "-map", "[v1out]", "-c:v:0", "libx264", "-b:v:0", "5M", "-maxrate:v:0", "5M",
-                                    "-minrate:v:0", "5M", "-bufsize:v:0", "10M", "-preset", "fast", "-g", "48",
-                                    "-sc_threshold", "0", "-keyint_min", "48", "-hls_segment_filename",
-                                    "stream_%v/data%02d.ts",
-                                    "-hls_base_url", "stream_0/",
-                                    // Resolución 480p
-                                    "-map", "[v2out]", "-c:v:1", "libx264", "-b:v:1", "3M", "-maxrate:v:1", "3M",
-                                    "-minrate:v:1", "3M", "-bufsize:v:1", "3M", "-preset", "fast", "-g", "48",
-                                    "-sc_threshold", "0", "-keyint_min", "48", "-hls_segment_filename",
-                                    "stream_%v/data%02d.ts",
-                                    "-hls_base_url", "stream_1/",
-                                    // Resolución 360p
-                                    "-map", "[v3out]", "-c:v:2", "libx264", "-b:v:2", "1M", "-maxrate:v:2", "1M",
-                                    "-minrate:v:2", "1M", "-bufsize:v:2", "1M", "-preset", "fast", "-g", "48",
-                                    "-sc_threshold", "0", "-keyint_min", "48", "-hls_segment_filename",
-                                    "stream_%v/data%02d.ts",
-                                    "-hls_base_url", "stream_2/"));
-                            lista.add("v:0,a:0");
-                            lista.add("v:1,a:1");
-                            lista.add("v:2,a:2");
-                            audio.addAll(Arrays.asList("-map", "a:0", "-c:a:0", "aac", "-b:a:0", "96k", "-ac", "2"));
-                            audio.addAll(Arrays.asList("-map", "a:0", "-c:a:1", "aac", "-b:a:1", "96k", "-ac", "2"));
-                            audio.addAll(Arrays.asList("-map", "a:0", "-c:a:2", "aac", "-b:a:2", "48k", "-ac", "2"));
-                        } else if (width >= 854 && height >= 480) {
-                            comando.add("[0:v]split=2[v1][v2]; [v1]copy[v1out]; [v2]scale=w=640:h=360[v2out]");
-                            maps.addAll(Arrays.asList(
-                                    // Resolución 480p
-                                    "-map", "[v1out]", "-c:v:0", "libx264", "-b:v:0", "3M", "-maxrate:v:0", "3M",
-                                    "-minrate:v:0", "3M", "-bufsize:v:0", "6M", "-preset", "fast", "-g", "48",
-                                    "-sc_threshold", "0", "-keyint_min", "48", "-hls_segment_filename",
-                                    "stream_%v/data%02d.ts",
-                                    "-hls_base_url", "stream_0/",
-                                    // Resolución 360p
-                                    "-map", "[v2out]", "-c:v:1", "libx264", "-b:v:1", "1M", "-maxrate:v:1", "1M",
-                                    "-minrate:v:1", "1M", "-bufsize:v:1", "2M", "-preset", "fast", "-g", "48",
-                                    "-sc_threshold", "0", "-keyint_min", "48", "-hls_segment_filename",
-                                    "stream_%v/data%02d.ts",
-                                    "-hls_base_url", "stream_1/"));
-                            lista.add("v:0,a:0");
-                            lista.add("v:1,a:1");
-                            audio.addAll(Arrays.asList("-map", "a:0", "-c:a:0", "aac", "-b:a:0", "96k", "-ac", "2"));
-                            audio.addAll(Arrays.asList("-map", "a:0", "-c:a:1", "aac", "-b:a:1", "48k", "-ac", "2"));
-                        } else {
-                            comando.add("[0:v]copy[v1out]");
-                            maps.addAll(Arrays.asList(
-                                    // Resolución 420p
-                                    "-map", "[v1out]", "-c:v:0", "libx264", "-b:v:0", "512k", "-maxrate:v:0", "512k",
-                                    "-minrate:v:0", "512k", "-bufsize:v:0", "1M", "-preset", "fast", "-g", "48",
-                                    "-sc_threshold", "0", "-keyint_min", "48", "-hls_segment_filename",
-                                    "stream_%v/data%02d.ts",
-                                    "-hls_base_url", "stream_0/"));
-                            lista.add("v:0,a:0");
-                            audio.addAll(Arrays.asList("-map", "a:0", "-c:a:3", "aac", "-b:a:3", "48k", "-ac", "2"));
-                        }
-
-                        // Construcción del comando FFmpeg
-                        List<String> ffmpegCommand = new ArrayList<>();
-                        ffmpegCommand.add("ffmpeg");
-                        ffmpegCommand.add("-i");
-                        ffmpegCommand.add(inputFileName);
-                        ffmpegCommand.add("-filter_complex");
-                        ffmpegCommand.add(comando.get(0)); // Se pasa como string
-
-                        // Añadir los mapeos de video y audio al comando
-                        ffmpegCommand.addAll(maps);
-                        ffmpegCommand.addAll(audio);
-
-                        // Opciones de salida de HLS
-                        ffmpegCommand.addAll(Arrays.asList(
-                                "-f", "hls",
-                                "-hls_time", "5",
-                                "-g", String.valueOf(fps * 5),
-                                "-hls_playlist_type", "vod",
-                                "-hls_flags", "independent_segments",
-                                "-hls_segment_type", "mpegts",
-                                "-master_pl_name", "master.m3u8",
-                                "-var_stream_map", String.join(" ", lista),
-                                "stream_%v.m3u8"));
-
+                    if (ffmpegCommand != null) {
                         // Ejecutar el comando FFmpeg
                         System.out.println("COMANDO FINAL: " + String.join(" ", ffmpegCommand));
                         ProcessBuilder processBuilder = new ProcessBuilder(ffmpegCommand);
@@ -264,7 +97,7 @@ public class StreamingService {
                         try (var reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
                             String line;
                             while ((line = reader.readLine()) != null) {
-                                System.out.println(line);
+                                System.out.println("FFmpeg: " + line);
                             }
                         }
 
@@ -275,11 +108,6 @@ public class StreamingService {
 
                         clase.setDireccion_clase(base.toString() + "/master.m3u8");
                         this.claseRepo.save(clase);
-
-                    } catch (IOException e) {
-                        System.err.println("Error en la entrada/salida: " + e.getMessage());
-                    } catch (Exception e) {
-                        System.err.println("Error en convertir el video: " + e.getMessage());
                     }
                 }
             }
@@ -661,7 +489,6 @@ public class StreamingService {
                         + resolutionPairs.get(i).getValue() + "[v" + (i + 1) + "out]";
             }
         }
-        System.out.println("Filtro: " + filtro);
         filters.add(filtro);
 
         for (int i = 0; i < resolutionPairs.size(); i++) {
@@ -696,16 +523,20 @@ public class StreamingService {
                     "-map", "a:0", "-c:a:" + i, "aac", "-b:a:" + i, audioBitrate, "-ac", "2"));
         }
 
-        System.out.println("Filtros: " + String.join(" ", filters));
-
         // Crea el comando FFmpeg
         List<String> ffmpegCommand = new ArrayList<>();
         ffmpegCommand = new ArrayList<>(List.of(
-                "ffmpeg", "-loglevel", "warning", "-re", "-i", inputFilePath,
+                "ffmpeg", "-loglevel", "warning"));
+        if (live) {
+            ffmpegCommand.add("-re");
+        }
+        ;
+        ffmpegCommand.addAll(List.of(
+                "-i", inputFilePath,
                 "-f", "hls",
                 "-hls_time", "5",
                 "-hls_playlist_type", hls_playlist_type,
-                "-hls_flags", hls_flags, // Asegura continuidad
+                "-hls_flags", hls_flags,
                 "-hls_segment_type", "mpegts",
                 "-filter_complex"));
         ffmpegCommand.addAll(filters);
