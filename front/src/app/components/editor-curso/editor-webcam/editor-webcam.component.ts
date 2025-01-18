475b92e21ee6657d89a3022df3cef4b023260f7d
giacca90 <giacca90mobile@gmail.com>
2025-01-18 10:16:28 +0100

    Cambiada la lógica del drag and drop para permitir el evento wheel

--- front/src/app/components/editor-curso/editor-webcam/editor-webcam.component.ts
+++ front/src/app/components/editor-curso/editor-webcam/editor-webcam.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
 	selector: 'app-editor-webcam',
@@ -7,13 +7,15 @@ import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular
 	templateUrl: './editor-webcam.component.html',
 	styleUrls: ['./editor-webcam.component.css'],
 })
-export class EditorWebcamComponent implements OnInit {
+export class EditorWebcamComponent implements OnInit, AfterViewInit {
 	videoDevices: MediaDeviceInfo[] = []; // Lista de dispositivos de video
 	audioDevices: MediaDeviceInfo[] = []; // Lista de dispositivos de audio
 	capturas: MediaStream[] = []; // Lista de capturas
 	videosElements: VideoElement[] = []; // Lista de elementos de video
 	dragVideo: VideoElement | null = null; // Video que se está arrastrando
 	dragPosition = { x: 0, y: 0 }; // Posición del ratón mientras se arrastra
+	canvas: HTMLCanvasElement | null = null;
+	context: CanvasRenderingContext2D | null = null;
 	@ViewChildren('videoElement') videoElements!: QueryList<ElementRef<HTMLVideoElement>>;
 
 	async ngOnInit() {
@@ -35,6 +37,25 @@ export class EditorWebcamComponent implements OnInit {
 		} catch (error) {
 			console.error('Error al acceder a los dispositivos:', error);
 		}
+	}
+
+	ngAfterViewInit(): void {
+		this.canvas = document.getElementById('salida') as HTMLCanvasElement;
+		this.context = this.canvas.getContext('2d');
+
+		const drawFrame = () => {
+			if (!this.canvas || !this.context) return;
+			this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+			this.videosElements.forEach((elemento) => {
+				if (!elemento.painted || !elemento.position || !this.context) return;
+				const videoWidth = (elemento.element.videoWidth * elemento.scale) / 5;
+				const videoHeight = (elemento.element.videoHeight * elemento.scale) / 5;
+				this.context.drawImage(elemento.element!, elemento.position.x, elemento.position.y, videoWidth, videoHeight);
+			});
+			requestAnimationFrame(drawFrame);
+		};
+
+		drawFrame(); // Comienza el bucle de renderizado
 	}
 
 	startMedias(devices: MediaDeviceInfo[]) {
@@ -225,6 +246,120 @@ export class EditorWebcamComponent implements OnInit {
 		}
 	}
 
+	mousedown(event: MouseEvent, deviceId: string): void {
+		console.log('Mousedown');
+		const videoElement = document.getElementById(deviceId) as HTMLVideoElement;
+		if (!videoElement) {
+			console.error('No hay videoElement');
+			return;
+		}
+		const ele = this.videosElements.find((el) => el.id === deviceId);
+		if (!ele || !this.canvas) {
+			console.error('No hay elemento');
+			return;
+		}
+
+		this.dragVideo = ele;
+
+		const ghost = videoElement.cloneNode(true) as HTMLVideoElement;
+		ghost.classList.add('ghost-video'); // Clase para estilizar el ghost
+		ghost.style.position = 'absolute';
+		ghost.style.pointerEvents = 'none'; // Para que no interfiera con eventos
+		ghost.style.zIndex = '1000';
+
+		// Ajustar dimensiones del ghost para que coincidan con el video original
+		ghost.style.width = `${videoElement.offsetWidth}px`;
+		ghost.style.height = `${videoElement.offsetHeight}px`;
+
+		// Copiar estilos clave
+		ghost.style.transform = getComputedStyle(videoElement).transform || 'none';
+		ghost.style.objectFit = getComputedStyle(videoElement).objectFit || 'contain';
+		ghost.style.fontFamily = getComputedStyle(videoElement).fontFamily || 'inherit';
+
+		// Copiar fuente y poner en marcha
+		ghost.srcObject = ele.element.srcObject;
+		ghost.load();
+
+		// Dimensiones del video
+		const elementWidth = videoElement.offsetWidth;
+		const elementHeight = videoElement.offsetHeight;
+		const offsetX = elementWidth / 2;
+		const offsetY = elementHeight / 2;
+
+		const updateGhostPosition = (x: number, y: number) => {
+			ghost.style.left = `${x - offsetX}px`;
+			ghost.style.top = `${y - offsetY}px`;
+		};
+
+		updateGhostPosition(event.clientX, event.clientY); // Posición inicial
+		document.body.appendChild(ghost);
+
+		// Evento para detectar `wheel`
+		const wheel = (wheelEvent: WheelEvent) => {
+			console.log('Wheel');
+			if (!this.dragVideo || !this.canvas) return;
+
+			const rect = this.canvas.getBoundingClientRect();
+			const isMouseOverCanvas: boolean = wheelEvent.clientX >= rect.left && wheelEvent.clientX <= rect.right && wheelEvent.clientY >= rect.top && wheelEvent.clientY <= rect.bottom;
+
+			if (isMouseOverCanvas) {
+				wheelEvent.preventDefault();
+				this.dragVideo.scale += wheelEvent.deltaY < 0 ? 0.05 : -0.05;
+				this.dragVideo.scale = Math.max(0.1, this.dragVideo.scale);
+			}
+		};
+
+		document.addEventListener('wheel', wheel, { passive: false });
+
+		// Evento para mover el ghost
+		const mousemove = (moveEvent: MouseEvent) => {
+			console.log('Mousemove');
+			try {
+				if (!this.dragVideo || !this.canvas) return;
+
+				updateGhostPosition(moveEvent.clientX, moveEvent.clientY);
+
+				const rect = this.canvas.getBoundingClientRect();
+				const isMouseOverCanvas: boolean = moveEvent.clientX >= rect.left && moveEvent.clientX <= rect.right && moveEvent.clientY >= rect.top && moveEvent.clientY <= rect.bottom;
+				console.log('Mouse over canvas: ', isMouseOverCanvas);
+			} catch (error) {
+				console.error('Error al mover el video: ', error);
+			}
+		};
+		document.addEventListener('mousemove', mousemove);
+
+		// Evento para soltar el ratón
+		const mouseup = (upEvent: MouseEvent) => {
+			console.log('MOUSEUP');
+			if (!this.dragVideo || !this.canvas) return;
+
+			const rect = this.canvas.getBoundingClientRect();
+			const isMouseOverCanvas: boolean = upEvent.clientX >= rect.left && upEvent.clientX <= rect.right && upEvent.clientY >= rect.top && upEvent.clientY <= rect.bottom;
+
+			if (isMouseOverCanvas) {
+				// Relación de escala entre el tamaño visual y el tamaño interno del canvas
+				const scaleX = this.canvas.width / rect.width;
+				const scaleY = this.canvas.height / rect.height;
+
+				// Ajustar la posición del ratón al tamaño interno del canvas
+				this.dragVideo.position = {
+					x: Math.round((upEvent.clientX - rect.left) * scaleX),
+					y: Math.round((upEvent.clientY - rect.top) * scaleY),
+				};
+				this.dragVideo.painted = true;
+			}
+
+			// Restaurar estado
+			this.dragVideo = null;
+			ghost.remove();
+			document.removeEventListener('mousemove', mousemove);
+			document.removeEventListener('wheel', wheel);
+			document.removeEventListener('mouseup', mouseup);
+		};
+
+		document.addEventListener('mouseup', mouseup);
+	}
+
 	dragStart(event: DragEvent, deviceId: string): void {
 		event.dataTransfer?.setData('deviceId', deviceId);
 		const videoElement = document.getElementById(deviceId) as HTMLVideoElement;
@@ -238,10 +373,13 @@ export class EditorWebcamComponent implements OnInit {
 			console.log('No hay videoElement');
 		}
 
-		// Elimina el efecto visual del ghosting
-		const img = new Image(); // Crea una imagen vacía
-		img.src = ''; // No asignamos ninguna fuente
-		event.dataTransfer?.setDragImage(img, 0, 0); // Aplica la imagen vacía
+		// Crear el elemento "ghost"
+		const ghost = videoElement.cloneNode(true) as HTMLVideoElement;
+		ghost.classList.add('ghost-video'); // Agregar clase CSS para estilizar el ghost
+		ghost.style.position = 'absolute';
+		ghost.style.pointerEvents = 'none'; // Para que no interfiera con otros eventos
+		ghost.style.zIndex = '1000';
+		document.body.appendChild(ghost);
 	}
 
 	dragOver(event: DragEvent) {
@@ -310,19 +448,6 @@ export class EditorWebcamComponent implements OnInit {
 			const drawX = this.dragPosition.x - videoWidth / 2;
 			const drawY = this.dragPosition.y - videoHeight / 2;
 			this.dragVideo.position = { x: drawX, y: drawY };
-
-			const drawFrame = () => {
-				context.clearRect(0, 0, canvas.width, canvas.height);
-				this.videosElements.forEach((elemento) => {
-					if (!elemento.painted || !elemento.position) return;
-					const videoWidth = (elemento.element.videoWidth * elemento.scale) / 5;
-					const videoHeight = (elemento.element.videoHeight * elemento.scale) / 5;
-					context.drawImage(elemento.element!, elemento.position.x, elemento.position.y, videoWidth, videoHeight);
-				});
-				requestAnimationFrame(drawFrame);
-			};
-
-			drawFrame(); // Comienza el bucle de renderizado
 			this.dragVideo = null; // Limpia el estado del video arrastrado
 		}
 	}
