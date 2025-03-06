3102ef7722c4a21da3027c86f511a519e0cdc1a4
giacca90 <giacca90mobile@gmail.com>
2025-03-06 22:03:56 +0100

    Corregidos los errores del carousel de profesores y de la animación del header

--- front/src/app/components/home/home.component.ts
+++ front/src/app/components/home/home.component.ts
@@ -6,11 +6,11 @@ import { InitService } from '../../services/init.service';
 import { UsuariosService } from '../../services/usuarios.service';
 
 @Component({
-    selector: 'app-home',
+	selector: 'app-home',
 	standalone: true,
-    imports: [],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.css'
+	imports: [],
+	templateUrl: './home.component.html',
+	styleUrl: './home.component.css',
 })
 export class HomeComponent {
 	vistaCursos: HTMLDivElement[] = [];
@@ -27,53 +27,55 @@ export class HomeComponent {
 	}
 
 	async carouselProfes() {
-		const carouselProfes: HTMLDivElement[] = [];
-		this.usuarioService.profes.forEach((profe: Usuario) => {
-			const div: HTMLDivElement = document.createElement('div') as HTMLDivElement;
-			div.classList.add('border', 'border-black', 'rounded-lg', 'flex', 'h-full', 'p-2', 'flex-1', 'opacity-0', 'transition-opacity', 'duration-1000');
+		setTimeout(async () => {
+			const carouselProfes: HTMLDivElement[] = [];
+			this.usuarioService.profes.forEach((profe: Usuario) => {
+				const div: HTMLDivElement = document.createElement('div') as HTMLDivElement;
+				div.classList.add('border', 'border-black', 'rounded-lg', 'flex', 'h-full', 'p-2', 'flex-1', 'opacity-0', 'transition-opacity', 'duration-1000');
 
-			const img: HTMLImageElement = document.createElement('img') as HTMLImageElement;
-			img.classList.add('h-full', 'w-auto', 'object-contain', 'mr-4');
-			img.src = profe.foto_usuario[0];
-			img.alt = 'profe';
-			div.appendChild(img);
+				const img: HTMLImageElement = document.createElement('img') as HTMLImageElement;
+				img.classList.add('h-full', 'w-auto', 'object-contain', 'mr-4');
+				img.src = profe.foto_usuario[0];
+				img.alt = 'profe';
+				div.appendChild(img);
 
-			const desc: HTMLDivElement = document.createElement('div') as HTMLDivElement;
-			desc.classList.add('flex', 'flex-col', 'flex-1', 'items-center', 'justify-center');
+				const desc: HTMLDivElement = document.createElement('div') as HTMLDivElement;
+				desc.classList.add('flex', 'flex-col', 'flex-1', 'items-center', 'justify-center');
 
-			const nombre: HTMLParagraphElement = document.createElement('p') as HTMLParagraphElement;
-			nombre.classList.add('text-blond', 'text-green-700', 'text-center');
-			nombre.textContent = profe.nombre_usuario.toString();
-			desc.appendChild(nombre);
+				const nombre: HTMLParagraphElement = document.createElement('p') as HTMLParagraphElement;
+				nombre.classList.add('text-blond', 'text-green-700', 'text-center');
+				nombre.textContent = profe.nombre_usuario.toString();
+				desc.appendChild(nombre);
 
-			const pres: HTMLParagraphElement = document.createElement('p') as HTMLParagraphElement;
-			pres.classList.add('text-center');
-			pres.textContent = profe.presentacion.toString();
-			desc.appendChild(pres);
+				const pres: HTMLParagraphElement = document.createElement('p') as HTMLParagraphElement;
+				pres.classList.add('text-center');
+				pres.textContent = profe.presentacion.toString();
+				desc.appendChild(pres);
 
-			div.appendChild(desc);
-			carouselProfes.push(div);
-		});
+				div.appendChild(desc);
+				carouselProfes.push(div);
+			});
 
-		const profes: HTMLDivElement = document.getElementById('profes') as HTMLDivElement;
-		let reverse: boolean = false;
-		while (carouselProfes.length > 0) {
-			const profe: HTMLDivElement | undefined = carouselProfes.shift();
-			if (profe) {
-				profes.innerHTML = '';
-				carouselProfes.push(profe);
-				if (reverse) {
-					profe.classList.add('flex-row-reverse');
+			const profes: HTMLDivElement = document.getElementById('profes') as HTMLDivElement;
+			let reverse: boolean = false;
+			while (carouselProfes.length > 0) {
+				const profe: HTMLDivElement | undefined = carouselProfes.shift();
+				if (profe) {
+					profes.innerHTML = '';
+					carouselProfes.push(profe);
+					if (reverse) {
+						profe.classList.add('flex-row-reverse');
+					}
+					profes.appendChild(profe);
+					this.cdr.detectChanges();
+					profe.classList.remove('opacity-0');
+					reverse = !reverse;
+					await this.delay(5000);
+					profe.classList.add('opacity-0');
+					await this.delay(1000);
 				}
-				profes.appendChild(profe);
-				this.cdr.detectChanges();
-				profe.classList.remove('opacity-0');
-				reverse = !reverse;
-				await this.delay(5000);
-				profe.classList.add('opacity-0');
-				await this.delay(1000);
 			}
-		}
+		}, 200);
 	}
 
 	delay(ms: number): Promise<void> {
